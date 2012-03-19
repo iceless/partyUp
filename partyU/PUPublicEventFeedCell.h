6cdbfedec4bb868e56e32a5cf3d4b672bc41ba21
Bing <iceless.bing@gmail.com>
2012-03-19 12:31:13 -0700

    use TableViewCell subclass for prototype cell
    instead of tag

---
+++ partyU/PUPublicEventFeedCell.h
@@ -0,0 +1,13 @@
+//
+//  PUPublicEventFeedCell.h
+//  partyU
+//
+//  Created by Jie Wu on 3/19/12.
+//  Copyright (c) 2012 NYU poly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PUPublicEventFeedCell : UITableViewCell
+
+@end
---
+++ partyU/PUPublicEventFeedCell.m
@@ -0,0 +1,29 @@
+//
+//  PUPublicEventFeedCell.m
+//  partyU
+//
+//  Created by Jie Wu on 3/19/12.
+//  Copyright (c) 2012 NYU poly. All rights reserved.
+//
+
+#import "PUPublicEventFeedCell.h"
+
+@implementation PUPublicEventFeedCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
