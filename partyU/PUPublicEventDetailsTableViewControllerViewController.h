49cbc7877baf1cf9a68d7def8c15e7c36e36ebee
Jie <ben.wujie@gmail.com>
2012-03-19 09:13:51 -0700

    add an empty segue for cell click
    nothing implement

---
+++ partyU/PUPublicEventDetailsTableViewControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PUPublicEventDetailsTableViewControllerViewController.h
+//  partyU
+//
+//  Created by Jay on 3/18/12.
+//  Copyright (c) 2012 NYU poly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PUPublicEventDetailsTableViewControllerViewController : UITableViewController
+
+@end
